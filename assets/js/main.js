ec36ba7caa1aaf42e8a7b169ef3d7df4209a14e4
kumelkalen <free.your.mind.fc@gmail.com>
2017-08-02 18:44:36 -0400

    Creamos el elemento ul fuera de la función para poder usarlo fuera de esta

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,6 +6,7 @@ div.appendChild(titulo);
 function MiembroSquad (nombre,edad){
 	this.nombre = nombre;
 	this.edad = edad ;
+	var lista = document.createElement('ul');
 	this.hobbies = function(a,b,c){
 		var hobbies = [a,b,c];
 		for(i=0;i<hobbies.length;i++){
@@ -14,6 +15,7 @@ function MiembroSquad (nombre,edad){
 			item.appendChild(hobbie);
 			lista.appendChild(item);
 			div.appendChild(lista);
+		}
 	}
 	var nombre = document.createTextNode("Nombre: "+this.nombre);
 	var pNombre = document.createElement('p');
