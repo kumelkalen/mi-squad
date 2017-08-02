f642065e04f5a07c4c79bd628fb774d7ea926d6f
kumelkalen <free.your.mind.fc@gmail.com>
2017-08-02 18:36:01 -0400

    Comenzamos la función constructora

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,3 +3,8 @@ var titulo = document.createElement("h1");
 var squadName = document.createTextNode("Squad 6");
 titulo.appendChild(squadName);
 div.appendChild(titulo);
+function MiembroSquad (nombre,edad,hobbies){
+	this.nombre = nombre;
+	this.edad = edad ;
+	this.hobbies = hobbies;
+}
